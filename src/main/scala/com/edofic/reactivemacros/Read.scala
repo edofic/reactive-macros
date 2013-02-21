0c12efbb7eb970cd8daa0f17ba414d24dfb3ea25
Andraz Bajt <edofic@gmail.com>
2013-02-21 17:46:36 +0100

    macro: full macroification of read

--- src/main/scala/com/edofic/reactivemacros/Read.scala
+++ src/main/scala/com/edofic/reactivemacros/Read.scala
@@ -2,6 +2,8 @@ package com.edofic.reactivemacros
 
 import language.experimental.macros
 import reflect.macros.Context
+import reactivemongo.bson.{BSONDocument, BSONValue}
+import reactivemongo.bson.handlers.BSONReader
 
 /**
  * User: andraz
@@ -10,9 +12,9 @@ import reflect.macros.Context
  */
 object Read {
 
-  def apply[A](map: Map[String,Any]): A = macro impl[A]
+  def apply[A]: BSONReader[A] = macro impl[A]
 
-  def impl[A: c.WeakTypeTag](c: Context)(map: c.Expr[Map[String,Any]]): c.Expr[A] = {
+  def impl[A: c.WeakTypeTag](c: Context): c.Expr[BSONReader[A]] = {
     import c.universe._
     def echo(a: Any) = c.echo(c.enclosingPosition, a.toString)
 
@@ -30,16 +32,22 @@ object Read {
       val reader = c.inferImplicitValue(typ)
       if(reader.isEmpty) c.abort(c.enclosingPosition, s"Implicit Reader for parameter '$param' not found")
 
-      val arg = Apply(Select(map.tree, "apply"), List(Literal(Constant(param.name.toString))))
+      val arg = Apply(Select(Ident(newTermName("map")), "apply"), List(Literal(Constant(param.name.toString))))
       Apply(Select(reader, "read"), List(arg))
     }
 
     val constructorTree = c.parse(constructor.fullName)
-    val a = Apply(constructorTree, values)
+    val creator = c.Expr[A](Apply(constructorTree, values))
+    echo("Generated code\n"+show(creator.tree))
 
-    val result = c.Expr[A](a)
-    echo("Generated code\n"+show(result.tree))
-    result
+    reify {
+      new BSONReader[A] {
+        def fromBSON(doc: BSONDocument): A = {
+          val map = doc.mapped
+          creator.splice
+        }
+      }
+    }
   }
 }
 
