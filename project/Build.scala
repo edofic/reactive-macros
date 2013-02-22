1610704add5e156b391f041b1111a93876cea223
Andraz Bajt <edofic@gmail.com>
2013-02-22 19:03:26 +0100

    macro tests moved to macros

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,10 @@ import Keys._
 object MacroBuild extends Build{
   val buildSettings = Defaults.defaultSettings ++ Seq(
     scalaVersion := "2.10.0",
+    libraryDependencies ++= Seq(
+      "org.reactivemongo" % "reactivemongo_2.10" % "0.8",
+      "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+    ),
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _)
   )
 
---
+++ src/test/scala/MacroTest.scala
@@ -0,0 +1,46 @@
+import com.edofic.reactivemacros.FormatBSON
+import org.scalatest.FunSuite
+import reactivemongo.bson.handlers.{BSONWriter, BSONReader}
+
+/**
+ * User: andraz
+ * Date: 2/22/13
+ * Time: 9:17 AM
+ */
+class MacroTest extends FunSuite{
+  def roundtrip[A](original: A, format: BSONReader[A] with BSONWriter[A]) = {
+    val serialized = format.toBSON(original)
+    val deserialized = format.fromBSON(serialized)
+    assert(original === deserialized)
+  }
+
+  test("nesting"){
+    implicit val personFormat = FormatBSON[Person]
+    val doc = Pet("woof", Person("john", "doe"))
+    roundtrip(doc, FormatBSON[Pet])
+  }
+
+  test("primitives"){
+    val doc = Primitives(1.2, "hai", true, 42, Long.MaxValue)
+    roundtrip(doc, FormatBSON[Primitives])
+  }
+
+  test("seq"){
+    val doc = WordLover("joe", Seq("hello", "world"))
+    roundtrip(doc, FormatBSON[WordLover])
+  }
+
+  test("option"){
+    val format = FormatBSON[Optional]
+    val some = Optional("some", Some("value"))
+    val none = Optional("none", None)
+    roundtrip(some, format)
+    roundtrip(none, format)
+  }
+}
+
+case class Person(firstName: String, lastName: String)
+case class Pet(name: String, owner: Person)
+case class Primitives(dbl: Double, str: String, bl: Boolean, int: Int, long: Long)
+case class WordLover(name: String, words: Seq[String])
+case class Optional(name: String, value: Option[String])
